77e9e89eeecdd2c698dfe6c7d2e68963fb70c1a2
kundan kumar <kundankumarkondil1999@gmail.com>
2024-07-03 20:18:34 +0530

    css change

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header className="bg-white py-4">
   <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-    <img src={logo} alt="Logo" className="h-10 sm:h-12" /> {/* Adjust height as needed */}
-    <nav className="hidden sm:flex items-center gap-8">
+    <img src={logo} alt="Logo" className="h-10 sm:h-12" /> 
+    <nav className="hidden sm:flex items-center gap-8" id="navbar-hamburger">
       <ul className="flex gap-8 text-[#6D6E71]">
         <li>Find Suppliers</li>
         <li className="flex items-center gap-2">
@@ -34,12 +34,21 @@ export default function Header() {
     </nav>
     {/* Mobile Menu Button for small screens */}
     <div className="sm:hidden">
-      <button className="px-3 py-2 border-2 border-[#00732F] text-[#00732F] rounded-md font-bold text-[13px]">
-        Menu
-      </button>
+      
+      <button data-collapse-toggle="#navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+      <span className="sr-only">Open main menu</span>
+      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+      </svg>
+    </button>
+    
     </div>
   </div>
 </header>
+
+
+
+
 
   );
 }
